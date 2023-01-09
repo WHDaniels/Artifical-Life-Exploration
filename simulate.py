c42d71abf51ec82bb140e754c64a5b784953e469
WHDaniels <whdaniels3@gmail.com>
2023-01-08 18:20:13 -0600

    loop through world

--- simulate.py
+++ simulate.py
@@ -1,5 +1,13 @@
 import pybullet as p
+import time
 
 physicsClient = p.connect(p.GUI)
+
+for n in range(1000):
+	p.stepSimulation()
+	time.sleep(1/60)
+	
+	if n % 50 == 0:
+		print(n)
 
 p.disconnect()
