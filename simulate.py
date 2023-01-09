6c029a04a1c0c1b22adf7a7a149c75ecdbfbc9a9
WHDaniels <whdaniels3@gmail.com>
2023-01-08 20:50:12 -0600

    end assignment 1

--- simulate.py
+++ simulate.py
@@ -1,8 +1,13 @@
 import pybullet as p
+import pybullet_data
 import time
 
 physicsClient = p.connect(p.GUI)
-p.loadSDF("box.sdf")
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
+p.setGravity(0, 0, -9.8)
+planeId = p.loadURDF("plane.urdf")
+robotId = p.loadURDF("body.urdf")
+p.loadSDF("world.sdf")
 
 for n in range(1000):
 	p.stepSimulation()
