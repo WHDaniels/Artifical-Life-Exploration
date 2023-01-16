0cf7ca670a95862d82609ef686b12ef055c32515
WHDaniels <whdaniels3@gmail.com>
2023-01-15 20:51:53 -0600

    random movement

--- simulate.py
+++ simulate.py
@@ -2,7 +2,8 @@ import pybullet as p
 import pybullet_data
 import time
 import pyrosim.pyrosim as pyrosim
-import numpy
+import numpy as np
+import random
 
 physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
@@ -12,14 +13,32 @@ robotId = p.loadURDF("body.urdf")
 p.loadSDF("world.sdf")
 pyrosim.Prepare_To_Simulate(robotId)
 
-ticks = 100
-backLegSensorValues = numpy.zeros(ticks)
-frontLegSensorValues = numpy.zeros(ticks)
+ticks = 500
+backLegSensorValues = np.zeros(ticks)
+frontLegSensorValues = np.zeros(ticks)
 
 for n in range(ticks):
 	p.stepSimulation()
 	backLegSensorValues[n] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
 	frontLegSensorValues[n] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
+
+	pyrosim.Set_Motor_For_Joint(
+	bodyIndex = robotId,
+	jointName = b'Torso_BackLeg',
+	controlMode = p.POSITION_CONTROL,
+	targetPosition = np.pi/(4*(random.random()-0.5)),
+	maxForce = 25
+	)
+
+	pyrosim.Set_Motor_For_Joint(
+	bodyIndex = robotId,
+	jointName = b'Torso_FrontLeg',
+	controlMode = p.POSITION_CONTROL,
+	targetPosition = np.pi/(4*(random.random()-0.5)),
+	maxForce = 25
+	)
+	print(4*(random.random()-0.5))
+
 	time.sleep(1/60)
 	
 	if n % 50 == 0:
@@ -27,5 +46,5 @@ for n in range(ticks):
 
 p.disconnect()
 
-numpy.save("data/backLegSensorValues.npy", backLegSensorValues)
-numpy.save("data/frontLegSensorValues.npy", frontLegSensorValues)
+np.save("data/backLegSensorValues.npy", backLegSensorValues)
+np.save("data/frontLegSensorValues.npy", frontLegSensorValues)
